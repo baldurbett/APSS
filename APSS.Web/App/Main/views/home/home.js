d349c90429885a1c8d41898e32a89c6f347896a2
baldurb <baldurbett@gmail.com>
2014-09-01 12:46:49 +0000

    Front Page Changes
    Changes to front page - changed HTML markup and added static Kendo UI
    widgets to home page.

--- APSS.Web/App/Main/views/home/home.js
+++ APSS.Web/App/Main/views/home/home.js
@@ -12,30 +12,61 @@
             $scope.screenResolution = new kendo.data.DataSource({
                 data: [{
                     category: "Asia",
-                    value: 53.8,
-                    color: "#9de219"
+                    value: 53.8                    
                 }, {
                     category: "Europe",
-                    value: 16.1,
-                    color: "#90cc38"
+                    value: 16.1                    
                 }, {
                     category: "Latin America",
-                    value: 11.3,
-                    color: "#068c35"
+                    value: 11.3                  
                 }, {
                     category: "Africa",
-                    value: 9.6,
-                    color: "#006634"
+                    value: 9.6                   
                 }, {
                     category: "Middle East",
-                    value: 5.2,
-                    color: "#004d38"
+                    value: 5.2                  
                 }, {
                     category: "North America",
-                    value: 3.6,
-                    color: "#033939"
+                    value: 3.6                   
                 }]
             });
+
+
+            $scope.mainGridOptions = {
+                dataSource: {
+                    type: "odata",
+                    transport: {
+                        read: "http://demos.telerik.com/kendo-ui/service/Northwind.svc/Employees"
+                    },
+                    pageSize: 5,
+                    serverPaging: true,
+                    serverSorting: true
+                },
+                sortable: true,
+                pageable: true,               
+                dataBound: function () {
+                    this.expandRow(this.tbody.find("tr.k-master-row").first());
+                },
+                columns: [{
+                    field: "FirstName",
+                    title: "First Name",
+                    width: "120px"
+                }, {
+                    field: "LastName",
+                    title: "Last Name",
+                    width: "120px"
+                }, {
+                    field: "Country",
+                    width: "120px"
+                }, {
+                    field: "City",
+                    width: "120px"
+                }, {
+                    field: "Title"
+                }]
+            };
+
+            
             
         }
     ]);
